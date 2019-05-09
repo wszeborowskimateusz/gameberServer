6e0a41011c00ed359c2532f536d99c279e50f380
Mateusz <wszeborowskimateusz@gmail.com>
2019-05-09 15:21:02 +0200

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
