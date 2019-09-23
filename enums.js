343acb0e2cbbd6090276c020b82108009fbae6b5
Korneliusz Karbowski <karbowski.korneliusz@gmail.com>
2019-09-23 12:54:41 +0200

    dodanie enuma dla clash

--- enums.js
+++ enums.js
@@ -5,12 +5,17 @@ module.exports.NotificationType = Object.freeze({
     FRIEND_REQUEST: 'friendship_request',
     NEW_MESSAGE: 'message_received',
     NEW_ACHIEVEMENT: 'achievement_received',
-    FRIEND_REQUEST_ACCEPTED: 'friendship_accepted'
+    FRIEND_REQUEST_ACCEPTED: 'friendship_accepted',
+    CLASH_REQUEST: 'clash_request',
+    CLASH_WON: 'clash_won',
+    CLASH_LOST: 'clash_lost',
+    CLASH_DRAW: 'clash_draw'
 });
 
 module.exports.NotificationImage = Object.freeze({
     NEW_MESSAGE: 'new_message.jpg',
-    FRIEND_REQUEST: 'friend_request.jpg'
+    FRIEND_REQUEST: 'friend_request.jpg',
+    CLASH_REQUEST: 'notifications/clash_request.jpg'
 });
 //#endregion
 
@@ -27,7 +32,8 @@ module.exports.BeginnersTestStatus = Object.freeze({
 module.exports.CategoryType = Object.freeze({
     BEGINNER_TEST: 'BT',
     BEGINNER: 'B',
-    NORMAL: 'N'
+    NORMAL: 'N',
+    CLASH: 'C'
 });
 //#endregion
 
