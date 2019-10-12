2f38875d6517b3d480f7038b04bbcf3f2474de9b
Korneliusz Karbowski <karbowski.korneliusz@gmail.com>
2019-10-12 16:19:42 +0200

    Poprawki zmiany statusu na  TestStarted + poprawka do powiadomień

--- routes/games.js
+++ routes/games.js
@@ -18,17 +18,14 @@ router.post('/check-answer', async function(req,res){
     try{
         await session.startTransaction();
 
-        const user = await db.User.findById(USER_ID);
-        if (user.beginners_test_status == enums.BeginnersTestStatus.TEST){
-            user.beginners_test_status = enums.BeginnersTestStatus.TEST_STARTED;
-            await user.save({ session });
-        }
-
         const isPassed = await db.User_Game.findOne({game_id: gameId, user_id: USER_ID});
         r.wasAlreadySolved = isPassed == null ? false : true;
 
-        const isCorrect = await db.Games.findOne({_id: gameId, correct_answer: answer});
-        if (isCorrect){
+        const game = await db.Games.
+            findById(gameId).
+            populate('category_id');
+
+        if (game.correct_answer == answer){
             if (!isPassed){
                 const newPassedGame = new db.User_Game({
                     game_id: gameId,
@@ -40,6 +37,14 @@ router.post('/check-answer', async function(req,res){
         }
         else
             r.isCorrect = false;
+
+        if (game.category_id.category_type == enums.CategoryType.BEGINNER_TEST) {
+            const user = await db.User.findById(USER_ID);
+            if (user.beginners_test_status == enums.BeginnersTestStatus.TEST){
+                user.beginners_test_status = enums.BeginnersTestStatus.TEST_STARTED;
+                await user.save({ session });
+            }
+        }
         
         await session.commitTransaction();
         res.status(200).json(r);
